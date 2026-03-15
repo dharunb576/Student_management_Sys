ee14f50d56a82eec9d620cecf1233a5cac81f630
Vignesh Dharun B <vigneshdharunb@gmail.com>
2026-03-15 11:55:11 +0530

    move frontend inside backend

--- student-management-backend/src/main.ts
+++ student-management-backend/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // ✅ Serve frontend static files
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   // ✅ Enable CORS
   app.enableCors({
